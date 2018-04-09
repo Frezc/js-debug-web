4a73ae113c0fe160a7ede40a51edd5f61327c2fa
PC Yang <504021398@qq.com>
2018-04-10 01:03:27 +0800

    complete unity sprite convert tool

---
+++ unity-sprite-to-pixi/readme.md
@@ -0,0 +1,13 @@
+### how to use
+1. add your sprite sheet to unity
+2. edit sprite by sprite editor (slice, named, ...)
+3. copy all content in your <image-file-name>.meta file and paste to left textarea
+4. fill you image file name
+5. click `>` button
+6. you can edit generated json content
+7. click `copy result` then paste to a *.json file
+8. load this json file by PIXI loader
+9. every time you edit this sprite sheet, just repeat 1 ~ 8
+
+### todo
+- [ ] export a command line tool to convert file
---
+++ unity-sprite-to-pixi/src/index.js
@@ -0,0 +1,74 @@
+import yaml from 'js-yaml';
+import JSONEditor from 'jsoneditor';
+
+var jsonEditor = new JSONEditor(document.getElementById('result'), {
+  modes: ['tree', 'code']
+});
+
+document.getElementById('copyBtn').addEventListener('click', function () {
+  copyToClipboard(JSON.stringify(jsonEditor.get(), null, 2))
+})
+
+document.getElementById('exec').addEventListener('click', function () {
+  try {
+    var inputEl = document.getElementById('input');
+    var doc = yaml.load(inputEl.value);
+
+    if (doc.TextureImporter.spriteSheet.sprites) {
+      var sprites = doc.TextureImporter.spriteSheet.sprites;
+      var frames = {};
+      var sheetHeight = sprites[0].rect.y + sprites[0].rect.height;
+      sprites.map(function (sprite) {
+        frames[sprite.name] = {
+          frame: {
+            x: sprite.rect.x + sprite.border.x,
+            y: sheetHeight - sprite.rect.y - sprite.rect.height + sprite.border.w,
+            w: sprite.rect.width - sprite.border.x - sprite.border.z,
+            h: sprite.rect.height - sprite.border.y - sprite.border.w
+          }
+        }
+      });
+      jsonEditor.set({
+        frames,
+        meta: {
+          image: document.getElementById('filename').value
+        }
+      });
+    }
+  } catch (e) {
+    alert('not valid meta file');
+    console.error(e);
+  }
+})
+
+function copyToClipboard(text) {
+  if (text.indexOf('-') !== -1) {
+    let arr = text.split('-');
+    text = arr[0] + arr[1];
+  }
+  var textArea = document.createElement("textarea");
+  textArea.style.position = 'fixed';
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = '0';
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'copied to clipboard successfully' : 'fail';
+    alert(msg);
+  } catch (err) {
+    alert('fail');
+    console.error(err);
+  }
+
+  document.body.removeChild(textArea);
+}
